30bb54a92a985f996a2ff9224a006fb692853957
Abhishek Yadav <95499180+ay27294@users.noreply.github.com>
2022-09-11 11:37:19 +0530

    Create ns_Assignment_App.js
    What is your dream project that you'd like to make after you learn React? Lets write them using React itself.

---
+++ ns_Assignment_App.js
@@ -0,0 +1,16 @@
+import React, { Component, useState } from 'react';
+import './styles/App.css';
+
+const App = () => {
+  return (
+    <div id="main">
+      <div data-ns-test="project-name">Amazon</div>
+      <div data-ns-test="project-description">Ecommerce</div>
+
+      <div data-ns-test="project-name">Google</div>
+      <div data-ns-test="project-description">Search Engine</div>
+    </div>
+  );
+};
+
+export default App;
